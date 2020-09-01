bd2364caa2e00463eddb997e68fe8e434016b9b9
unknown <2360376884@qq.com>
2020-09-01 21:17:10 +0800

    reach no.38

--- src/com/zavier/pratice/Main.java
+++ src/com/zavier/pratice/Main.java
@@ -1046,6 +1046,14 @@ class Solution37 {
     }
 }
 
+// 题目：二叉树的深度
+class Solution38 {
+    public int TreeDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(TreeDepth(root.left), TreeDepth(root.right)) + 1;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Solution21_1 s = new Solution21_1();
