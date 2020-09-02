36e706226e6ac213b98970e01a7aecc2d0a39366
unknown <2360376884@qq.com>
2020-09-02 20:20:39 +0800

    reach no.40

--- src/com/zavier/pratice/Main.java
+++ src/com/zavier/pratice/Main.java
@@ -1049,8 +1049,91 @@ class Solution37 {
 // 题目：二叉树的深度
 class Solution38 {
     public int TreeDepth(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
         return Math.max(TreeDepth(root.left), TreeDepth(root.right)) + 1;
+    }
+}
+
+// 题目：平衡二叉树
+// 思路1：分别求每个节点的的树的高度，只有满足每个节点的子树高度差小于等于1才是平衡二叉树
+class Solution39 {
+    public boolean IsBalanced_Solution(TreeNode root) {
+        if (root == null) return true;
+
+        int leftDepth = treeDepth(root.left);
+        int rithtDepth = treeDepth(root.right);
+
+        if (Math.abs(leftDepth - rithtDepth) > 1) return false;
+
+        return IsBalanced_Solution(root.left) && IsBalanced_Solution(root.right);
+    }
+
+    int treeDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(treeDepth(root.left), treeDepth(root.right)) + 1;
+    }
+}
+
+// 题目：平衡二叉树
+// 思路2:修改求树高度的函数，当左右子树高度差大于1时停止递归
+class Solution39_1 {
+    public boolean IsBalanced_Solution(TreeNode root) {
+        return treeDepth(root) >= 0;
+    }
+
+    int treeDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = treeDepth(root.left);
+        if (left == -1) return -1;
+        int right = treeDepth(root.right);
+        if (right == -1) return -1;
+
+        return Math.abs(left - right) <= 1 ? Math.max(left, right) + 1 : -1;
+    }
+}
+
+// 题目：数组中只出现一次的数字
+/* 思路：
+首先：位运算中异或的性质：两个相同数字异或=0，一个数和0异或还是它本身。
+
+当只有一个数出现一次时，我们把数组中所有的数，依次异或运算，最后剩下的就是落单的数，因为成对儿出现的都抵消了。
+
+依照这个思路，我们来看两个数（我们假设是AB）出现一次的数组。我们首先还是先异或，剩下的数字肯定是A、B异或的结果，
+这个结果的二进制中的1，表现的是A和B的不同的位。我们就取第一个1所在的位数，假设是第3位，接着把原数组分成两组，
+分组标准是第3位是否为1。如此，相同的数肯定在一个组，因为相同数字所有位都相同，而不同的数，肯定不在一组。
+然后把这两个组按照最开始的思路，依次异或，剩余的两个结果就是这两个只出现一次的数字。
+ */
+class Solution40 {
+    public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
+        int xorResult = 0;
+        if (array.length == 2) {
+            num1[0] = array[0];
+            num2[0] = array[1];
+            return;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            xorResult ^= array[i];
+        }
+        int index = findFirst1Index(xorResult);
+        for (int i = 0; i < array.length; i++) {
+            if((array[i]>>index&1)==0){
+                num1[0] ^= array[i];
+            } else {
+                num2[0] ^= array[i];
+            }
+        }
+    }
+
+    int findFirst1Index(int a) {
+        int index = 0;
+        while (a != 0) {
+            if ((a >> index & 1) == 1) {
+                return index;
+            }
+            index++;
+        }
+        return index;
     }
 }
 
